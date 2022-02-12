d39a8ae4f9a2a193ee903bdeffd61c4c6346ff8d
Anshul Rana <anshulranacoc2002@gmail.com>
2022-02-13 01:11:02 +0530

    Update AirplaneController.cs

--- Assets/Scripts/AirplaneController.cs
+++ Assets/Scripts/AirplaneController.cs
@@ -5,32 +5,28 @@ using UnityEngine;
 
 public class AirplaneController : MonoBehaviour
 {
-     public float speedAir = 50.0f;
-
-    // void Update()
-    // {
-    //     transform.position += transform.forward*Time.deltaTime*speedAir;
-    //     speedAir -= transform.forward.y * Time.deltaTime *10.0f;
-    //     transform.Rotate(-Input.GetAxis("Vertical2"),0.0f,-Input.GetAxis("Horizontal2"));
-    // }
-
+    public float speedAir = 50.0f;
     public float turnSpeed;
     private float horizontalInput;
     private float forwardInput;
     public float speedLand = 20f;
 
     private Rigidbody rigidBody;
-    public float forwardThrustForce = 40.0f;
+    public float rotateSpeed;
+    public float jumpSpeed;
 
     public Camera mainCamera;
     public Camera CockpitCamera;
     public KeyCode switchKey;
 
-    void Start () {
+    Vector3 m_EulerAngleVelocity;
+    void Start()
+    {
         rigidBody = GetComponent<Rigidbody>();
-	}
+    }
 
-    void Update(){
+    void Update()
+    {
         //Land Controls
         horizontalInput = Input.GetAxis("Horizontal1");
         forwardInput = Input.GetAxis("Vertical1");
@@ -39,20 +35,27 @@ public class AirplaneController : MonoBehaviour
         //Rotate Plane left and right
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
-        if(Input.GetKeyDown(switchKey))
+        if (Input.GetKeyDown(switchKey))
         {
             mainCamera.enabled = !mainCamera.enabled;
             CockpitCamera.enabled = !CockpitCamera.enabled;
         }
+        m_EulerAngleVelocity = new Vector3(0, 0, -Input.GetAxis("Horizontal2"));
 
-        if (Input.GetButton("Jump"))
+        if (Input.GetKey("w"))
         {
-            transform.position += transform.forward*Time.deltaTime*speedAir;
-            speedAir -= transform.forward.y * Time.deltaTime *10.0f;
-            transform.Rotate(-Input.GetAxis("Vertical2"),0.0f,-Input.GetAxis("Horizontal2"));
+            rigidBody.MovePosition(rigidBody.position + transform.forward * Time.deltaTime * speedAir);
         }
+        if (Input.GetKey("w") && Input.GetKey("space"))
+        {
+            rigidBody.MovePosition(rigidBody.position + transform.up * jumpSpeed + transform.forward * Time.deltaTime * speedAir);
+        }
+        speedAir -= transform.forward.y * Time.deltaTime * 10.0f;
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime * rotateSpeed);
+        rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+
     }
-    
+
 }
-    
+
 
